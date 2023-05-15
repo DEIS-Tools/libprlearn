704b62bb62bc500667069b7492d7c19f4c2775eb
Peter G. Jensen <root@petergjoel.dk>
2023-05-15 22:09:52 +0200

    tmp

--- src/RefinementTree.h
+++ src/RefinementTree.h
@@ -55,7 +55,7 @@ namespace prlearn {
         struct qdata_t {
             avg_t _midpoint;
             avg_t _lmid, _hmid;
-            qvar_t _lowq, _highq;
+            rqvar_t _lowq, _highq;
             splitfilter_t _splitfilter;
         };
 
