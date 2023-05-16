bc272dc5e4380ba08ab843057d2aa6c266ea6f10
Peter G. Jensen <root@petergjoel.dk>
2023-05-16 09:53:57 +0200

    removing unused learning-rate arg

--- src/propts.h
+++ src/propts.h
@@ -1,21 +1,21 @@
 /*
  * Copyright Peter G. Jensen
- *  
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* 
+/*
  * File:   propts.h
  * Author: Peter G. Jensen
  *
@@ -32,7 +32,6 @@ namespace prlearn {
     // primed for Q-learning
 
     struct propts_t {
-        size_t _q_learn_rate = std::numeric_limits<size_t>::max(); // only QLearning
         double _upper_t = 1.75;
         double _lower_t = 0.15;
         double _ks_limit = 0.25;
