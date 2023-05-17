e9dec45748e3ac9a5cd6f4246c234c9c1d31a1c9
Peter G. Jensen <root@petergjoel.dk>
2023-05-17 11:45:51 +0200

    frac lr

--- src/structs.cpp
+++ src/structs.cpp
@@ -65,7 +65,7 @@ namespace prlearn {
         }
         else
         {
-            const auto frac = 1.0/std::max(std::log(_cnt), std::min(2.0, _cnt));
+            const auto frac = 1.0/std::max(std::sqrt(_cnt), std::min(2.0, _cnt));
             _avg = ((1.0-frac)*_avg) + (frac * d);
             auto nvar = std::pow(d - _avg, 2.0) - _variance;
             ++_cnt;
